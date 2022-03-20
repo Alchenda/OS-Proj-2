69278b72c31dceac53044750a5d48782e256975f
Charles D. Webb-McClain <89601203+Alchenda@users.noreply.github.com>
2022-03-19 23:01:03 -0400

    Update prods_cons_MT.cpp
    I am having trouble on line 51, if you have trouble understanding what I am attempting just shoot me a message on Disc

--- prods_cons_MT.cpp
+++ prods_cons_MT.cpp
@@ -6,9 +6,13 @@
 //
 #include <queue>
 #include <fstream>
+#include <iostream>
+#include <time.h>
 #include "prods_cons_MT.hpp"
-
-//pretty sure this entire constructor is useless
+using namespace std;
+extern queue<int> buffer; //shared queue
+extern int bufferSize; //shared size of buffer
+//this constructor is actually not used
 prods_cons_MT::prods_cons_MT(int myID, char PorC, int amount){
     prodOrConNumber = myID; //unique number for each producer/consumer
     ID = PorC; //identify if producer/consumer
@@ -19,10 +23,14 @@ prods_cons_MT::prods_cons_MT(){
     ID = NULL;
     toProdOrCon = 0;
 }
-void prods_cons_MT::produce(){
-    
+void prods_cons_MT::produce(pthread_t prod){
+    randomProduce = rand() % 6; //we can change this later for true randomness, it is good for now for seeing the test cases to match close to his
+    while(buffer.size() < bufferSize){
+        buffer.push(randomProduce);
+    }
+    prod -> wait(); //I think you can see what I am trying to do here, I am trying to get the producer just to wait. Going to be honest I have been at this for a few hours now and not sure how to get this to work
 }
-void prods_cons_MT::consume(){
+void prods_cons_MT::consume(pthread_t con){
     
 }
 int prods_cons_MT::GetProdOrConNumber(){
@@ -42,4 +50,10 @@ void prods_cons_MT::SetAmount(int amount){
 }
 void prods_cons_MT::SetProdOrCon(char PorC){
     ID = PorC; //Identify if producer or consumer
+}
+void prods_cons_MT::ProducerStartPrint(){ //print message for when a producer is created
+    cout << "P" << prodOrConNumber << ": Producing " << toProdOrCon << " values" << endl;
+}
+void prods_cons_MT::ConsumerStartPrint(){ //print message for when a consumer is created
+    cout << "C" << prodOrConNumber << ":  Consuming" << toProdOrCon << " values" << endl;
 }
