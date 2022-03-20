afd7d14d906f4b6a339e28df36910a6477cad7a4
Charles D. Webb-McClain <89601203+Alchenda@users.noreply.github.com>
2022-03-19 23:01:33 -0400

    Updated header

--- prods_cons_MT.hpp
+++ prods_cons_MT.hpp
@@ -7,14 +7,14 @@
 
 #ifndef prods_cons_MT_hpp
 #define prods_cons_MT_hpp
-
+#include <pthread.h>
 #include <stdio.h>
 class prods_cons_MT{
 public:
     prods_cons_MT();
-    prods_cons_MT(int myID, char PorC, int amount); //not used I believe
-    void consume(); //function to preform consumption
-    void produce(); //function to preform production
+    prods_cons_MT(int myID, char PorC, int amount);
+    void consume(pthread_t con); //function to preform consumption
+    void produce(pthread_t prod); //function to preform production
     int GetProdOrConNumber(); //retrieve what number producer or consumer it is
     char GetID(); //retrieve whether it is a producer or consumer
     int GetAmount(); //retrieve the amount to produce or consume
@@ -26,6 +26,7 @@ public:
 private:
     int prodOrConNumber; //unique number for each producer/consumer
     char ID; //identify if producer/consumer
-    int toProdOrCon; // the amount to be produced or consumed
+    int toProdOrCon; //the amount to be produced or consumed
+    int randomProduce; //random integer that is to be produced by each producer to buffer
 };
 #endif /* prods_cons_MT_hpp */
