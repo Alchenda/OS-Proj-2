8064eacbecf529e6852e42429f3197f365e2ee91
florinda-i-martinez <florinda.i.martinez@gmail.com>
2022-03-21 22:24:43 -0400

    each thread runs in Create function without needing to create array

--- main.cpp
+++ main.cpp
@@ -11,17 +11,46 @@
 #include <queue>
 #include <fstream>
 #include "prods_cons_MT.cpp"
+#include <unistd.h>
 
 using namespace std;
 queue<int> buffer;
 mutex lock;
-int bufferSize;
+int bufferSize, pThread, cThread;;
+
+
+typedef struct {
+    int threadNum;
+} threadID;
+
+void *CreateProducer(void *prodArg) {
+
+    threadID *args = (threadID *) prodArg;
+    cout << "P" << args->threadNum << ": Producing " << "CONST 4" << " values" << endl;
+
+    return nullptr;
+}
+
+void *CreateConsumer(void *consArg) {
+
+    threadID *args = (threadID *) consArg;
+    cout << "C" << args->threadNum << ": Consumer " << "CONST 3" << " values" << endl;
+
+    return nullptr;
+}
+
+
+
 
 int main(int argc, const char * argv[]) {
-    int pThread, cThread;
+
+    threadID args = {-5};
+
     bufferSize = stoi(argv[1]); // size of buffer
-    pThread = stoi(argv[2]); // umber of producers to be produced
+    pThread = stoi(argv[2]); // number of producers to be produced
     cThread = stoi(argv[3]); //number of consumers to be produced
+
+    /*
     //****************** Dynamic Memory Allocation *******************
     prods_cons_MT *Producers; //Dynamic memory allocation of Producers
     prods_cons_MT *Consumers; //Dynamic memory allocation of Consumers
@@ -31,22 +60,30 @@ int main(int argc, const char * argv[]) {
     Consumers = new prods_cons_MT[cThread]; //create an array of the number of consumers
     prod = new pthread_t[pThread]; // create an array of producer threads
     con = new pthread_t[cThread]; // create an array of consumer threads
-    //****************** Object Creation *******************
+     */
+    pthread_t producer, consumer;
+
+
+
+
+
+
+
+
+    //****************** Thread Creation *******************
     //Right when an object is created it will begin consuming/producing right away
     for (int i = 0; i < pThread; ++i) { //create all of the producers
-        Producers[i].SetID(i); //which number the producer is
-        Producers[i].SetAmount(bufferSize * 2); //this is to ensure over production
-        Producers[i].SetProdOrCon('P'); //indicate it is a producer
-        cout << "Main: started producer " << i;
-        Producers[i].ProducerStartPrint();
-        Producers[i].produce(prod[i]); //producer thread begins production
+
+        args = {i};
+        cout << "Main: started producer " << i << endl;
+        pthread_create(&producer, nullptr, CreateProducer, &args);
     }
-    for (int i = 0; i < cThread; ++i){ //create all of the consumers
-        Consumers[i].SetID(i); //which number the consumer is
-        Consumers[i].SetAmount(cThread); //amount to consume
-        Consumers[i].SetProdOrCon('C'); //indicate it is a consumer
-        cout << "Main: started consumer " << i;
-        Consumers[i].consume(con[i]); //consumer thread begins consuming
+
+    for (int i = 0; i < cThread; ++i) { //create all of the consumers
+        args = {i};
+        cout << "Main: started consumer " << i << endl;
+        pthread_create(&consumer, nullptr, CreateConsumer, &args);
     }
+    sleep(1);
     return 0;
 }
--- prods_cons_MT.hpp
+++ prods_cons_MT.hpp
@@ -11,22 +11,14 @@
 #include <stdio.h>
 class prods_cons_MT{
 public:
-    prods_cons_MT();
     prods_cons_MT(int myID, char PorC, int amount);
-    void consume(pthread_t con); //function to preform consumption
-    void produce(pthread_t prod); //function to preform production
-    int GetProdOrConNumber(); //retrieve what number producer or consumer it is
-    char GetID(); //retrieve whether it is a producer or consumer
-    int GetAmount(); //retrieve the amount to produce or consume
-    void ProducerStartPrint(); //prints out required start message of producer
-    void ConsumerStartPrint(); //prints out required start message of consumer
-    void SetID(int myID);
-    void SetAmount(int amount);
-    void SetProdOrCon(char PorC);
+
+
+
 private:
     int prodOrConNumber; //unique number for each producer/consumer
     char ID; //identify if producer/consumer
     int toProdOrCon; //the amount to be produced or consumed
-    int randomProduce; //random integer that is to be produced by each producer to buffer
 };
+
 #endif /* prods_cons_MT_hpp */
