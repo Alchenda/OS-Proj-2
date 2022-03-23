c8578f479103a1fcefc075532db444bb77d5cf7a
Charles D. Webb-McClain <89601203+Alchenda@users.noreply.github.com>
2022-03-23 01:27:22 -0400

    Final update to monitor.cpp file
    There are a few print statements that need to be adjusted, you should they have just an "x" in them. Please adjust those when you have a chance tomorrow, I have a few examples in there with using the "threadNum" integer for thread identification. The algorithm for tracking the queue rotation is donated with qTrack for the Consumer, and prodTrack for the producer. 
    Please double check to make sure I am not missing any global variables

--- prods_cons_MTT.cpp
+++ prods_cons_MTT.cpp
@@ -14,34 +14,48 @@
 using namespace std;
 extern pthread_cond_t NAME;
 extern pthread_mutex_t lock1;
-extern vector<int> buffer;
-extern int bufferSize, pThread, cThread;
-void Produce(int count){ //I think we need to pass the thread ID here for printing purposes
-    int randomNumber = rand() % (11);
+extern queue<int> buffer;
+extern int bufferSize, pThread, cThread, qTrack, prodTrack, extraConsume;
+extern bool specialConsumer;
+
+void Produce(int count, int threadNum){
+    prodTrack = (prodTrack + 1) % (bufferSize + 1); //I believe these should be the same algorithm for tracking the que rotation position
+    int randomNumber = rand() % (11); //can be changed for true random up to you
     pthread_mutex_lock(&lock1);
     while(bufferSize == buffer.size()){
-        cout << "Producer is waiting" << endl; // block to full buffer?
+        cout << "Producer " << threadNum << " is waiting" << endl;
         pthread_cond_wait(&NAME, &lock1);
     }
     //critical section
-    buffer.push_back(randomNumber);
+    cout << "P" << threadNum << ": Writing " << randomNumber << " to position " << prodTrack << endl;
+    buffer.push(randomNumber);
     pthread_mutex_unlock(&lock1); //unlock the buffer
     pthread_cond_signal(&NAME); //wake the threads that are waiting to access the buffer
     if(count == 0){
         cout << "producer x done producing" << endl;
     } else{
-        Produce(count - 1);
-
-
+        Produce(count - 1, threadNum);
     }
     
 
 }
-void Consume(){
+void Consume(int count, int threadNum){
+    qTrack = (qTrack + 1) % (bufferSize + 1); //for tracking the position that is being removed, although it actually just always removes from the front of the queue, it gives perception of the position it came from
     pthread_mutex_lock(&lock1);
     while (buffer.empty() == true) {
         pthread_cond_wait(&NAME, &lock1);
+        cout << "Consumer x is waiting" << endl;
     }
     //critical section
-    pthread_mutex_unlock(&lock1);
+    cout << "C" << threadNum << ": Reading " << buffer.front() << " from position " << qTrack << endl;
+    buffer.pop();
+    pthread_mutex_unlock(&lock1); //unlock the buffer
+    pthread_cond_signal(&NAME); //wake the threads that are waiting to access the buffer
+    
+    if (count == 0) {
+        cout << "Consumer " << threadNum << " done consuming" << endl;
+    } else{
+        Consume(count - 1, threadNum);
+    }
 }
+
