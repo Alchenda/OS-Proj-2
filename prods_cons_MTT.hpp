c38c76fb04eaa3d38ec16adc63eb72a7c20e3ada
Charles D. Webb-McClain <89601203+Alchenda@users.noreply.github.com>
2022-03-23 01:28:24 -0400

    Final push to monitor.hpp file
    I made some notes in main that you will need to add a few prototypes in here for moving the main functions into the monitor.cpp
    Thank you,
    -Danny

--- prods_cons_MTT.hpp
+++ prods_cons_MTT.hpp
@@ -9,8 +9,8 @@
 #define prods_cons_MTT_hpp
 
 #include <stdio.h>
-void Produce(int count);
-void Consumer();
+void Produce(int count, int threadNum);
+void Consumer(int count, int threadNum);
 void *CreateProducer(void *prodArg);
 void *CreateConsumer(void *consArg);
 
