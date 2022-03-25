8f6bb67f88d8f4c5b38ab48104b34c6f2734b276
Charles D. Webb-McClain <89601203+Alchenda@users.noreply.github.com>
2022-03-24 23:40:06 -0400

    Update prods_cons_MT.cpp
    fixed consume and produce

--- prods_cons_MT.cpp
+++ prods_cons_MT.cpp
@@ -1,59 +1,61 @@
 //
-//  prods_cons_MT.cpp
-//  OSProj2
+//  prods_cons_MTT.cpp
+//  remakeeeee
 //
-//  Created by Danny Webb-McClain on 3/17/22.
+//  Created by Danny Webb-McClain on 3/22/22.
 //
+#include <iostream>
+#include <pthread.h>
+#include <mutex>
 #include <queue>
 #include <fstream>
-#include <iostream>
-#include <time.h>
-#include "prods_cons_MT.hpp"
+#include <unistd.h>
+#include "prods_cons_MTT.hpp"
 using namespace std;
-extern queue<int> buffer; //shared queue
-extern int bufferSize; //shared size of buffer
-//this constructor is actually not used
-prods_cons_MT::prods_cons_MT(int myID, char PorC, int amount){
-    prodOrConNumber = myID; //unique number for each producer/consumer
-    ID = PorC; //identify if producer/consumer
-    toProdOrCon = amount; // the amount to be produced or consumed
-}
-prods_cons_MT::prods_cons_MT(){
-    prodOrConNumber = 0;
-    ID = NULL;
-    toProdOrCon = 0;
-}
-void prods_cons_MT::produce(pthread_t prod){
-    randomProduce = rand() % 6; //we can change this later for true randomness, it is good for now for seeing the test cases to match close to his
-    while(buffer.size() < bufferSize){
-        buffer.push(randomProduce);
+extern pthread_cond_t IS_EMPTY, IS_FULL;
+extern pthread_mutex_t lock1;
+extern queue<int> buffer;
+extern int bufferSize, pThread, cThread, qTrack, prodTrack, extraConsume;
+extern bool specialConsumer;
+
+void Produce(int count, int threadNum){
+    prodTrack = (prodTrack + 1) % (bufferSize + 1); //I believe these should be the same algorithm for tracking the que rotation position
+    int randomNumber = rand() % (11); //can be changed for true random up to you
+    pthread_mutex_lock(&lock1);
+    while(bufferSize == buffer.size()){
+        cout << "Producer " << threadNum << " is waiting" << endl;
+        pthread_cond_wait(&IS_FULL, &lock1);
     }
-    prod -> wait(); //I think you can see what I am trying to do here, I am trying to get the producer just to wait. Going to be honest I have been at this for a few hours now and not sure how to get this to work
-}
-void prods_cons_MT::consume(pthread_t con){
+    //critical section
+    cout << "P" << threadNum << ": Writing " << randomNumber << " to position " << prodTrack << endl;
+    buffer.push(randomNumber);
+    pthread_mutex_unlock(&lock1); //unlock the buffer
+    pthread_cond_signal(&IS_FULL); //wake the producers that queue is no longer full
+    if(count == 0){
+        cout << "producer x done producing" << endl;
+    } else{
+        Produce(count - 1, threadNum);
+    }
     
+
 }
-int prods_cons_MT::GetProdOrConNumber(){
-    return prodOrConNumber;
+void Consume(int count, int threadNum){
+    qTrack = (qTrack + 1) % (bufferSize + 1); //for tracking the position that is being removed, although it actually just always removes from the front of the queue, it gives perception of the position it came from
+    pthread_mutex_lock(&lock1);
+    while (buffer.empty() == true) {
+        pthread_cond_wait(&IS_EMPTY, &lock1);
+        cout << "Consumer x is waiting" << endl;
+    }
+    //critical section
+    cout << "C" << threadNum << ": Reading " << buffer.front() << " from position " << qTrack << endl;
+    buffer.pop();
+    pthread_mutex_unlock(&lock1); //unlock the buffer
+    //Wake the consumers that have been set to sleep over empty queue
+    pthread_cond_signal(&IS_EMPTY);
+    if (count == 0) {
+        cout << "Consumer " << threadNum << " done consuming" << endl;
+    } else{
+        Consume(count - 1, threadNum);
+    }
 }
-char prods_cons_MT::GetID(){
-    return ID;
-}
-int prods_cons_MT::GetAmount(){
-    return toProdOrCon;
-}
-void prods_cons_MT::SetID(int myID){
-    prodOrConNumber = myID; //unique number of producer or consumer
-}
-void prods_cons_MT::SetAmount(int amount){
-    toProdOrCon = amount; //amount to produce only applies to producers
-}
-void prods_cons_MT::SetProdOrCon(char PorC){
-    ID = PorC; //Identify if producer or consumer
-}
-void prods_cons_MT::ProducerStartPrint(){ //print message for when a producer is created
-    cout << "P" << prodOrConNumber << ": Producing " << toProdOrCon << " values" << endl;
-}
-void prods_cons_MT::ConsumerStartPrint(){ //print message for when a consumer is created
-    cout << "C" << prodOrConNumber << ":  Consuming" << toProdOrCon << " values" << endl;
-}
+
